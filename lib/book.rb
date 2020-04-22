a40a4c5b8eeff6a725c2ca4b574737c645688f7e
agdsarge <agdsargeant@gmail.com>
2020-04-22 15:35:41 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
 
+    attr_accessor :author, :title, :genre, :page_count
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
